45dafff2abe86594d24bc6ef2bf34eadf64c6428
McNichol <mcnichol.m@gmail.com>
2017-02-18 15:04:06 -0600

    Add git related tools and dependencies, default global configuration

--- git/install.sh
+++ git/install.sh
@@ -1,2 +1,14 @@
-echo "Setup Git"
-sleep 5s
+echo "############################"
+echo "# Running Git Setup Module #"
+echo "############################"
+sleep 1s
+
+sudo apt-get -y install git \
+    tig                         #Git CLI Commit Browser
+
+#My custom configuration values
+git config --global user.name "McNichol"
+git config --global user.email mcnichol.m@gmail.com
+git config --global core.editor "vim"
+
+
