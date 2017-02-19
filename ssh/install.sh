107e699081a450f8f6f2f128f2f0d8140c26bfac
McNichol <mcnichol.m@gmail.com>
2017-02-19 00:35:53 -0600

    Create ssh module

--- ssh/install.sh
+++ ssh/install.sh
@@ -1,1 +1,7 @@
 sudo apt-get -y install openssh-server
+
+sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.default-template
+sudo chmod 666 /etc/ssh/sshd_config.default-template
+
+sudo systemctl restart ssh
+
