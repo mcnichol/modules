91415c4df8fb847e3ad2aa5a2f76b863f5a5c4ba
McNichol <mcnichol.m@gmail.com>
2017-02-19 11:12:51 -0600

    run docker as sudo

--- concourse/run-concourse.sh
+++ concourse/run-concourse.sh
@@ -2,8 +2,8 @@
 export CONCOURSE_EXTERNAL_URL=http://$(ifconfig enp5s0f2 | grep -Eo 'inet ([0-9]*\.){3}[0-9]*' | cut -d ' ' -f 2):8080
 echo "Setting CONCOURSE_EXTERNAL_URL to $CONCOURSE_EXTERNAL_URL" 
 
-cd ~/workspace/concourse
+cd ~/workspace/modules/concourse
 echo "Running \`docker-compose up\` on $(pwd)/docker-compose.yml" 
 sleep 1s
 
-docker-compose up -d
+sudo docker-compose up -d
