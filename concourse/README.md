67548dec86c3ff74128c6b08f13a4c0a49e64c97
McNichol <mcnichol.m@gmail.com>
2017-02-19 22:57:44 -0600

    Add commented out keygen creation for concourse
    +Functionality to run from any directory
    +Readme showing variables and usage statement for concourse module

---
+++ concourse/README.md
@@ -0,0 +1,8 @@
+# Concourse Module
+
+## Variables
+`PIPELINE_USER` - The Concourse Login Username and Postgres Database Username
+`PIPELINE_PASS` - The Concourse Login Password and Postgres Database Password
+
+## Usage
+`sudo PIPELINE_USER={USER_NAME} PIPELINE_PASS={PASSWORD} ./run-concourse.sh`
--- concourse/run-concourse.sh
+++ concourse/run-concourse.sh
@@ -1,7 +1,23 @@
 export CONCOURSE_EXTERNAL_URL=http://$(ifconfig enp5s0f2 | grep -Eo 'inet ([0-9]*\.){3}[0-9]*' | cut -d ' ' -f 2):8080
 echo "Setting CONCOURSE_EXTERNAL_URL to $CONCOURSE_EXTERNAL_URL" 
 
-cd ~/workspace/modules/concourse
+ORIGIN_DIR="$(pwd)"
+SCRIPT_DIR="$(dirname $0)"
+
+FULL_PATH="$ORIGIN_DIR/$SCRIPT_DIR"
+
+#mkdir -p keys/web keys/worker
+#
+#ssh-keygen -t rsa -f ./keys/web/tsa_host_key -N ''
+#ssh-keygen -t rsa -f ./keys/web/session_signing_key -N ''
+#
+#ssh-keygen -t rsa -f ./keys/worker/worker_key -N ''
+#
+#cp ./keys/worker/worker_key.pub ./keys/web/authorized_worker_keys
+#cp ./keys/web/tsa_host_key.pub ./keys/worker
+
+cd $FULL_PATH
+
 echo "Running \`docker-compose up\` on $(pwd)/docker-compose.yml" 
 sleep 1s
 
