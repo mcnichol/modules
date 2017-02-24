a33df3f80e7a4528c65697060dc5f8b981deee2b
McNichol <mcnichol.m@gmail.com>
2017-02-23 22:13:16 -0600

    Create pcfdev module and install

---
+++ pcf-dev/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sudo apt-get -y install unzip
+
+curl -fsSL https://github.com/mcnichol/file-sources/blob/master/pcf-dev-v0.24.0-linux.zip?raw=true > pcf-dev.zip
+
+rm pcfdev-v*
+unzip pcf-dev.zip && rm pcf-dev.zip
+rm pcfdev-v*
+
+./pcfdev-v*
