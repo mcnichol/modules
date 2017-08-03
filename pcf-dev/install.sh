b0393f1994c4c4c28132de081ad8ceb8cd70fdcc
McNichol <mcnichol.m@gmail.com>
2017-08-02 21:34:34 -0500

    Install virtualbox with pcf-dev

--- pcf-dev/install.sh
+++ pcf-dev/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt-get -y install unzip
+sudo apt-get -y install unzip virtualbox
 
 curl -fsSL https://github.com/mcnichol/file-sources/blob/master/pcf-dev-v0.24.0-linux.zip?raw=true > pcf-dev.zip
 
@@ -9,3 +9,4 @@ unzip pcf-dev.zip && rm pcf-dev.zip
 rm pcfdev-v*
 
 ./pcfdev-v*
+
