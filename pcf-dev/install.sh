a357d76a58315a265ff2fee4e4f1c54df405e4de
McNichol <mcnichol.m@gmail.com>
2017-09-03 18:58:21 -0500

    Correct typo in PCF-DEV link

--- pcf-dev/install.sh
+++ pcf-dev/install.sh
@@ -2,11 +2,10 @@
 
 sudo apt-get -y install unzip virtualbox
 
-curl -fsSL https://github.com/mcnichol/file-sources/blob/master/pcf-dev-v0.24.0-linux.zip?raw=true > pcf-dev.zip
+curl -fsSL https://github.com/mcnichol/file-sources/blob/master/pcfdev-v0.24.0-linux.zip?raw=true > pcf-dev.zip
 
 rm pcfdev-v*
 unzip pcf-dev.zip && rm pcf-dev.zip
-rm pcfdev-v*
 
 ./pcfdev-v*
 
