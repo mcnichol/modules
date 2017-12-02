b6384d6036ff2d25b11457cf929a7a05cc1d2579
Michael McNichol <mmcnichol@gogoair.com>
2017-12-01 19:03:28 -0600

    Customize concourse for Mac or Linux

--- concourse/run-concourse.sh
+++ concourse/run-concourse.sh
@@ -1,4 +1,5 @@
-export CONCOURSE_EXTERNAL_URL=http://$(ifconfig enp5s0f2 | grep -Eo 'inet ([0-9]*\.){3}[0-9]*' | cut -d ' ' -f 2):8080
+#export CONCOURSE_EXTERNAL_URL=http://$(ifconfig enp5s0f2 | grep -Eo 'inet ([0-9]*\.){3}[0-9]*' | cut -d ' ' -f 2):8080
+export CONCOURSE_EXTERNAL_URL=http://$(ifconfig | grep en0 -A 5 | grep -i "inet " | awk '{print $2}'):8080
 echo "Setting CONCOURSE_EXTERNAL_URL to $CONCOURSE_EXTERNAL_URL" 
 
 ORIGIN_DIR="$(pwd)"
@@ -6,15 +7,15 @@ SCRIPT_DIR="$(dirname $0)"
 
 FULL_PATH="$ORIGIN_DIR/$SCRIPT_DIR"
 
-#mkdir -p keys/web keys/worker
-#
-#ssh-keygen -t rsa -f ./keys/web/tsa_host_key -N ''
-#ssh-keygen -t rsa -f ./keys/web/session_signing_key -N ''
-#
-#ssh-keygen -t rsa -f ./keys/worker/worker_key -N ''
-#
-#cp ./keys/worker/worker_key.pub ./keys/web/authorized_worker_keys
-#cp ./keys/web/tsa_host_key.pub ./keys/worker
+mkdir -p keys/web keys/worker
+
+ssh-keygen -t rsa -f ./keys/web/tsa_host_key -N ''
+ssh-keygen -t rsa -f ./keys/web/session_signing_key -N ''
+
+ssh-keygen -t rsa -f ./keys/worker/worker_key -N ''
+
+cp ./keys/worker/worker_key.pub ./keys/web/authorized_worker_keys
+cp ./keys/web/tsa_host_key.pub ./keys/worker
 
 cd $FULL_PATH
 
