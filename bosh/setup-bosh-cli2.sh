30dd0205da48070279fe9b4e75f1ceae538f36ab
Michael McNichol <mmcnichol@gogoair.com>
2017-12-04 09:37:42 -0600

    Script director vm setup
    * Reference folder from BASH_SOURCE

--- bosh/setup-bosh-cli2.sh
+++ bosh/setup-bosh-cli2.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR=${BASH_SOURCE%/*}
 source "../_common_scripts/config/styles.sh"
 source "../_common_scripts/config/functions.sh"
 
@@ -29,3 +30,29 @@ if [ $(echo $THIS_PLATFORM) == "Darwin" ]; then
 fi
 
 source "bosh-cli-dependencies.sh"
+
+# Setup Director VM
+
+git clone https://github.com/cloudfoundry/bosh-deployment $SCRIPT_DIR/bosh-deployment
+mkdir -p $SCRIPT_DIR/deployments/vbox
+cd $SCRIPT_DIR/deployments/vbox
+
+bosh create-env ../../bosh-deployment/bosh.yml \
+  --state ./state.json \
+  -o ../../bosh-deployment/virtualbox/cpi.yml \
+  -o ../../bosh-deployment/virtualbox/outbound-network.yml \
+  -o ../../bosh-deployment/bosh-lite.yml \
+  -o ../../bosh-deployment/bosh-lite-runc.yml \
+  -o ../../bosh-deployment/jumpbox-user.yml \
+  --vars-store ./creds.yml \
+  -v director_name="Bosh Lite Director" \
+  -v internal_ip=192.168.50.6 \
+  -v internal_gw=192.168.50.1 \
+  -v internal_cidr=192.168.50.0/24 \
+  -v outbound_network_name=NatNetwork
+
+bosh alias-env vbox -e 192.168.50.6 --ca-cert <(bosh int ./creds.yml --path /director_ssl/ca)
+export BOSH_CLIENT=admin
+export BOSH_CLIENT_SECRET=`bosh int ./creds.yml --path /admin_password`
+
+bosh -e vbox env
