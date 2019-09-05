4990b8f58415e10acf80d7cd32fb66d8ecb18972
Michael McNichol <mcnichol.m@gmail.com>
2019-09-05 08:43:48 -0400

    Concourse updates

---
+++ concourse/REQUIREMENTS.md
@@ -0,0 +1,7 @@
+# Required Dependencies 
+A list of dependencies necessary for executing the concourse install
+
+## Docker
+This is required for leveraging `docker-compose` which orchestrates the installation of PostGres and Concourse
+
+Installl with `brew cask install docker` or by running `./install-deps.sh`
--- concourse/run-concourse.sh
+++ concourse/run-concourse.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 #export CONCOURSE_EXTERNAL_URL=http://$(ifconfig enp5s0f2 | grep -Eo 'inet ([0-9]*\.){3}[0-9]*' | cut -d ' ' -f 2):8080
 export CONCOURSE_EXTERNAL_URL=http://$(ifconfig | grep en0 -A 5 | grep -i "inet " | awk '{print $2}'):8080
 echo "Setting CONCOURSE_EXTERNAL_URL to $CONCOURSE_EXTERNAL_URL" 
